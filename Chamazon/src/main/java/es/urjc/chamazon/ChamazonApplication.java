604e2055beef76a06f7e458c0ec2908c89a0cb94
M44ri0 <167449725+M44ri0@users.noreply.github.com>
2025-02-11 10:49:28 +0100

    Inicio

---
+++ Chamazon/src/main/java/es/urjc/chamazon/ChamazonApplication.java
@@ -0,0 +1,13 @@
+package es.urjc.chamazon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChamazonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChamazonApplication.class, args);
+    }
+
+}
