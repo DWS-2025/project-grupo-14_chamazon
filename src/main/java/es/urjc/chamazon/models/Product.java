6ac47f474d68e39329d7591d64117c97b2ac7749
CapitanPescanova <v.espinosar.2023@alumnos.urjc.es>
2025-03-18 10:37:52 +0100

    Merge cambios bien hechos

--- src/main/java/es/urjc/chamazon/models/Product.java
+++ src/main/java/es/urjc/chamazon/models/Product.java
@@ -1,22 +1,20 @@
 package es.urjc.chamazon.models;
 
-
-import jakarta.persistence.*;
-
 import java.util.List;
+import jakarta.persistence.*;
 
 @Entity
 public class Product {
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
+    
     private Long id;
     private String name;
     private Float price;
     private String description;
     private String image;
     private Float rating;
-
 
     @ManyToOne
     private User user;
@@ -27,81 +25,62 @@ public class Product {
     @ManyToMany
     private List<ShoppingCar> shoppingCarList;
 
-    public Product() {
+    public Product() { }
 
+    public Product(String name, Float price, String description, String image, Float rating) {
+        this.name = name;
+        this.price = price;
+        this.description = description;
+        this.image = image;
+        this.rating = rating;
     }
 
-    //GETTERS AND SETTERS//
 
     public Long getId() {
         return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
     }
 
     public String getName() {
         return name;
     }
 
-    public void setName(String name) {
-        this.name = name;
-    }
-
     public Float getPrice() {
         return price;
-    }
-
-    public void setPrice(Float price) {
-        this.price = price;
     }
 
     public String getDescription() {
         return description;
     }
 
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
     public String getImage() {
         return image;
-    }
-
-    public void setImage(String image) {
-        this.image = image;
     }
 
     public Float getRating() {
         return rating;
     }
 
+    public void setId(Long id) {
+        this.id = id;
+    }
+    
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setPrice(Float price) {
+        this.price = price;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
     public void setRating(Float rating) {
         this.rating = rating;
-    }
-
-    public User getUser() {
-        return user;
-    }
-
-    public void setUser(User userPr) {
-        this.user = userPr;
-    }
-
-    public List<Category> getCategoryList() {
-        return categoryList;
-    }
-
-    public void setCategoryList(List<Category> categoryList) {
-        this.categoryList = categoryList;
-    }
-
-    public List<ShoppingCar> getShoppingCarList() {
-        return shoppingCarList;
-    }
-
-    public void setShoppingCarList(List<ShoppingCar> shoppingCarList) {
-        this.shoppingCarList = shoppingCarList;
     }
 }
