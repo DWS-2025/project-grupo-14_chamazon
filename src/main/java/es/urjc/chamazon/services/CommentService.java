1cc11123dced6e04575e2ad78e77a97be87b9c75
Beatriz <beatriz.sanz.granados@gmail.com>
2025-03-20 13:22:50 +0100

    Creación de CommentService

---
+++ src/main/java/es/urjc/chamazon/services/CommentService.java
@@ -0,0 +1,37 @@
+package es.urjc.chamazon.services;
+
+import es.urjc.chamazon.models.Comment;
+import es.urjc.chamazon.repositories.CommentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class CommentService  {
+
+    @Autowired
+    private CommentRepository commentRepository;
+
+    //Method to find all comments
+    public List<Comment> findAll() {
+        return commentRepository.findAll();
+    }
+    
+    //Method to find a comment by its id if it exists
+    public Optional<Comment> findById(Long id) {
+        return commentRepository.findById(id);
+    }
+
+    //Method to save a comment
+    public Comment save(Comment comment) {
+        return commentRepository.save(comment);
+    }
+
+    //Method to delete a comment by its id
+    public void deleteById(Long id) {
+        commentRepository.deleteById(id);
+    }
+
+}
