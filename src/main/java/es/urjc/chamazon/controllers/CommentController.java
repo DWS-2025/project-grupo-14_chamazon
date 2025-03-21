474c14b61842353357f94809dc3c0f7231d2d967
Beatriz <beatriz.sanz.granados@gmail.com>
2025-03-21 22:53:43 +0100

    Corrección en CommentController y creación de vistas
    Se modificaron rutas del CommentController para poder mapear bien y se creó la vista comments para poder tener una lista de comentarios (que requerirá posteriores actualizaciones para enlazar bien con producto) y se creó comment

--- src/main/java/es/urjc/chamazon/controllers/CommentController.java
+++ src/main/java/es/urjc/chamazon/controllers/CommentController.java
@@ -7,35 +7,62 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Optional;
+import java.util.List;
+
+//CRUD operations for comments: Create, Read, Update, Delete
+
 @Controller
-@RequestMapping("/comments")
+@RequestMapping("/commentView") //This part of the path to access the other methods
 public class CommentController {
 
     @Autowired
     private CommentService commentService;
 
-    @GetMapping
-    public String getAllComments(Model model) {
-        model.addAttribute("comments", commentService.findAll());
+
+    //Create Operations: method addCommentForm(GET) and addComment(POST) to add a new comment
+    @GetMapping("/add")
+    public String addCommentForm(Model model) {
+        model.addAttribute("comment", new Comment());
         return "comment/addNewComment";
+    }
+
+    @PostMapping("/add")
+    public String addComment(@ModelAttribute Comment comment) {
+        commentService.save(comment);
+        return "redirect:/commentView/comments";
+    }
+
+
+    //Read Operations: method getAllComments(GET) to get all comments and getCommentById(GET) to get
+    @GetMapping("/comments")
+    public String getAllComments(Model model) {
+        List<Comment> comments = commentService.findAll();
+        model.addAttribute("comments", comments);
+        return "comment/comments";
     }
 
     @GetMapping("/{id}")
     public String getCommentById(@PathVariable Long id, Model model) {
-        model.addAttribute("comment", commentService.findById(id).orElse(null));
-        return "comment/addNewComment";
+        Optional<Comment> comment = commentService.findById(id);
+        if (comment.isPresent()) {
+            model.addAttribute("comment", comment.get());
+            return "comment/comment";
+        } else {
+            return "error/error";
+        }
     }
 
-    @PostMapping
-    public String createComment(@ModelAttribute Comment comment) {
-        commentService.save(comment);
-        return "redirect:/comments";
-    }
 
+    //Update Operations: method getEditCommentPage(GET) and updateComment(POST) to update a comment
     @GetMapping("/edit/{id}")
-    public String getEditCommentPage(@PathVariable Long id, Model model) {
-        model.addAttribute("comment", commentService.findById(id).orElse(null));
-        return "comment/editComment";
+    public String getEditCommentPage(@PathVariable Optional<Long> id, Model model) {
+        if (id.isPresent()) {
+            model.addAttribute("comment", commentService.findById(id.get()).orElse(null));
+            return "comment/editComment";
+        } else {
+            return "redirect:/commentView/add";
+        }
     }
 
     @PostMapping("/edit/{id}")
@@ -45,15 +72,19 @@ public class CommentController {
             comment.setComment(commentDetails.getComment());
             comment.setRating(commentDetails.getRating());
             comment.setUser(commentDetails.getUser());
-            comment.setProduct(commentDetails.getProduct());
+            //comment.setProduct(commentDetails.getProduct());
             commentService.save(comment);
         }
-        return "redirect:/comments";
+        return "redirect:/commentView/comments";
     }
 
-    @GetMapping("/delete/{id}")
+
+    //Delete Operation: method deleteComment(POST) to delete a comment (delete with a button in the comment view)
+    @PostMapping("/delete/{id}")
     public String deleteComment(@PathVariable Long id) {
         commentService.deleteById(id);
-        return "redirect:/comments";
+        return "redirect:/commentView/comments";
     }
+
 }
+
