47f7e02f736480a2a3ec4ebd24f4b08a51e57889
Beatriz <beatriz.sanz.granados@gmail.com>
2025-03-25 11:11:47 +0100

    Update CommentRepository

--- src/main/java/es/urjc/chamazon/repositories/CommentRepository.java
+++ src/main/java/es/urjc/chamazon/repositories/CommentRepository.java
@@ -1,8 +1,14 @@
 package es.urjc.chamazon.repositories;
 
+import es.urjc.chamazon.models.Product;
+
 import  es.urjc.chamazon.models.Comment;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface CommentRepository  extends JpaRepository<Comment, Long> {
 
+    Optional <List <Comment>> findByProduct_Id(Long productId); //Metodo que tendrá que ser llamado en el servicio para obtener los comentarios de un producto
 }
