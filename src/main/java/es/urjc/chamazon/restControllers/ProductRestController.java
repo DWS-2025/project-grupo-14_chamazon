f3da54fca64c55f0a43f038db144649cf62e4402
M44ri0 <167449725+M44ri0@users.noreply.github.com>
2025-04-05 20:39:24 +0200

    0.9.1restdtov2

--- src/main/java/es/urjc/chamazon/restControllers/ProductRestController.java
+++ src/main/java/es/urjc/chamazon/restControllers/ProductRestController.java
@@ -1,6 +1,5 @@
 package es.urjc.chamazon.restControllers;
 
-
 import es.urjc.chamazon.dto.ProductDTO;
 import es.urjc.chamazon.dto.ProductMapper;
 import es.urjc.chamazon.models.Product;
@@ -10,6 +9,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
 import java.net.URI;
 import java.util.Collection;
 import java.util.Optional;
@@ -26,12 +26,12 @@ public class ProductRestController {
     @Autowired
     ProductMapper productMapper;
 
-    @GetMapping("/products/")
+    @GetMapping("/")
     public Collection<ProductDTO> getProducts() { // same as findall
         return productService.getProducts();
     }
 
-    @GetMapping("/products/{id}")
+    @GetMapping("/{id}")
     public ResponseEntity<ProductDTO> getProduct(@PathVariable long id) { // same as findById
         ProductDTO product = productService.getProduct(id);
         if (product != null) {
@@ -41,19 +41,17 @@ public class ProductRestController {
         }
     }
 
-    @PostMapping("/products/") // same as save when the user has added a new product
-    public ResponseEntity<ProductDTO> createProduct(@RequestBody ProductDTO productDTO, @RequestParam MultipartFile imageFile) {
+    @PostMapping("/") // same as save when the user has added a new product
+    public ResponseEntity<ProductDTO> createProduct(@RequestBody ProductDTO productDTO,
+            @RequestParam MultipartFile imageFile) {
         productService.save(productDTO, imageFile);
 
-        URI location = fromCurrentRequest()
-                .path("/{id}")
-                .buildAndExpand(productDTO.id())
-                .toUri();
+        URI location = fromCurrentRequest().path("/{id}").buildAndExpand(productDTO.id()).toUri();
 
-        return ResponseEntity.ok(productDTO);
+        return ResponseEntity.created(location).body(productDTO);
     }
 
-    @DeleteMapping("/products/{id}")
+    @DeleteMapping("/{id}")
     public ResponseEntity<ProductDTO> deleteProduct(@PathVariable long id) {
         ProductDTO product = productService.getProduct(id);
 
@@ -65,11 +63,40 @@ public class ProductRestController {
         }
     }
 
-    @PutMapping("/products/{id}")
-    public ResponseEntity<ProductDTO> updateProduct(@PathVariable long id, @RequestBody ProductDTO newProductDTO) {
-        Optional<Product> existingProduct = productService.findById(id);
-        if (existingProduct.isPresent()) {
-            newProductDTO.setId(id);
+    @PutMapping("/{id}")
+    public ResponseEntity<ProductDTO> updateProduct(@PathVariable long id, @RequestBody ProductDTO newProductDTO,
+            @RequestParam(required = false) MultipartFile imageFile) {
+        Optional<Product> existingProductOptional = productService.findById(id);
+        if (existingProductOptional.isPresent()) {
+            Product existingProduct = existingProductOptional.get();
+            // convert DTO to entity making sure it uses the service update method
+            Product newProduct = productMapper.toProduct(newProductDTO);
+            try {
+                ProductDTO updatedProductDTO = productService.update(existingProduct, newProduct, null);
+                return ResponseEntity.ok(updatedProductDTO);
+            } catch (IOException e) {
+                return ResponseEntity.badRequest().build();
+            }
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @PostMapping("/{id}/image")
+    public ResponseEntity<Object> uploadImage(@PathVariable long id, @RequestParam MultipartFile imageFile) {
+        Optional<Product> existingProductOptional = productService.findById(id);
+        if (existingProductOptional.isPresent()) {
+            try {
+                URI location = fromCurrentRequest().build().toUri();
+
+                Product existingProduct = existingProductOptional.get();
+
+                ProductDTO updatedProductDTO = productService.update(existingProduct, null, imageFile);
+
+                return ResponseEntity.created(location).build();
+            } catch (IOException e) {
+                return ResponseEntity.badRequest().build();
+            }
         } else {
             return ResponseEntity.notFound().build();
         }
