36f8c46a29977a380977567d02462210f2fb1706
M44ri0 <167449725+M44ri0@users.noreply.github.com>
2025-04-06 22:12:13 +0200

    borrado de cosas

--- src/main/java/es/urjc/chamazon/dto/ProductMapper.java
+++ src/main/java/es/urjc/chamazon/dto/ProductMapper.java
@@ -1,16 +1,10 @@
 package es.urjc.chamazon.dto;
 import es.urjc.chamazon.models.Product;
 import org.mapstruct.Mapper;
-import org.mapstruct.Mapping;
-import org.springframework.stereotype.Component;
-
-import java.util.List;
 import java.util.Collection;
 
 
 @Mapper(componentModel = "spring")
-@Component
-
 public interface ProductMapper {
     ProductDTO toDTO(Product product);
     Product toProduct(ProductDTO productDTO);
